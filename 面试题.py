e4806d1c7658382169ccccd31ad7b5b711cadd9e
HanYiran <815520326@qq.com>
2018-01-03 20:54:21 +0800

    面试题练习

---
+++ 面试题.py
@@ -0,0 +1,12 @@
+# def num1():
+#     return [lambda x :i*x for i in range(4)]
+# def num2():
+#     return (lambda x :i*x for i in range(4))
+#
+#
+#
+# print([m(2) for m in num1()])
+# print([m(2) for m in num2()])
+
+
+lambda lis : [x*x for x in range(1,10)]
